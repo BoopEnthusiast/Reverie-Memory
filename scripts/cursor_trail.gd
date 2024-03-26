b783eeb70e0dd5b5891ee3cc9738c89a965935a7
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-03-26 18:41:41 +1300

    Nice cursor trail
    I made this so I can stay here longer with reason so I can stay awake until at least 10pm so when I have to wake up at 8 I have a decent sleep instead of unwillingly waking up at 3am bc I went to bed at 7pm and being unable to go back to bed, haunted by regrets

--- scripts/cursor_trail.gd
+++ scripts/cursor_trail.gd
@@ -1,11 +1,29 @@
 extends Line2D
 
+@onready var dot = $Dot
+@onready var timer = $Timer
 
-# Called when the node enters the scene tree for the first time.
+var point_count: int
+
+
 func _ready():
-	pass # Replace with function body.
+	point_count = points.size()
+	for point in points:
+		point = get_local_mouse_position()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	dot.global_position = get_global_mouse_position()
+	for i in range(point_count - 1, -1, -1):
+		if i > 0:
+			points[i] = points[i - 1]
+		else:
+			points[0] = get_local_mouse_position()
+	await get_tree().create_timer(0.1, true, true).timeout
+	dot.global_position = get_global_mouse_position()
+	for i in range(point_count - 1, -1, -1):
+		if i > 0:
+			points[i] = points[i - 1]
+		else:
+			points[0] = get_local_mouse_position()
