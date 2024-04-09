f49d612d19eb3bf387d80c262285235a2fb1a9a9
Blulowy <140657391+Blulowy@users.noreply.github.com>
2024-04-09 16:16:46 +1200

    a

--- scripts/drag_simbol.gd
+++ scripts/drag_simbol.gd
@@ -1,9 +1,9 @@
-extends Panel
+extends AnimatableBody2D
 class_name Symbol
 var is_mouse_on = false
 var is_dragging_this = false
 @export var symbol : String
-@onready var collision_shape_2d = $AnimatableBody2D/CollisionShape2D
+@onready var collision_shape_2d = $CollisionShape2D
 
 func _on_mouse_entered():
 	is_mouse_on = true
@@ -13,7 +13,6 @@ func _on_mouse_exited():
 	is_mouse_on = false
 
 func _physics_process(delta):
-	global_position.y -= delta * 50
 	if is_mouse_on or SingletonNode.is_dragging:
 		if Input.is_action_pressed("click"):
 			SingletonNode.is_dragging = true
@@ -21,11 +20,7 @@ func _physics_process(delta):
 			is_dragging_this = true
 		if is_dragging_this:
 			
-			global_position = get_global_mouse_position() - size/2
+			global_position = get_global_mouse_position() - collision_shape_2d.shape.size/2
 		if Input.is_action_just_released("click"):
 			SingletonNode.is_dragging = false
 			is_dragging_this = false
-			
-	
-
-
--- scripts/socket_for_draggable.gd
+++ scripts/socket_for_draggable.gd
@@ -1,16 +1,10 @@
 extends Area2D
 var what_body
+@onready var collision_shape_2d = $CollisionShape2D
 
 
 
 
-func _on_body_entered(body):
-	print (body)
-	if body is AnimatableBody2D:
-		what_body = body.get_parent()
-		print (what_body)
-		
-		
 	
 
 
@@ -18,5 +12,10 @@ func _on_body_exited(body):
 	what_body = null 
 	
 func _physics_process(delta):
+	
 	if Input.is_action_just_released("click") and what_body:
 		what_body.global_position = global_position
+
+
+func _on_body_entered(body):
+	print("D'DAYYY")
