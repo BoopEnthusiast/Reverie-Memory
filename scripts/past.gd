9d13702c8c1cfd8d42dae2619d8bdadac2df9273
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-04-15 23:47:35 +1200

    I don't knwo why audio isn't working

--- scripts/past.gd
+++ scripts/past.gd
@@ -6,3 +6,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	homework_reminder.play()
+
+
+func _on_homework_reminder_finished():
+	print("why not?")
+	homework_reminder.play()
