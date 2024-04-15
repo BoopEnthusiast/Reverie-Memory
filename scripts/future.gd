cf4f5c3e7595ab02491f2d0f3b45b9fdc04ad74f
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-04-16 00:08:53 +1200

    reminders

--- scripts/future.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-@onready var call_mom_reminder = $CallMomReminder
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	call_mom_reminder.play()
--- scripts/past.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _on_homework_reminder_finished():
-	print("why not?")
