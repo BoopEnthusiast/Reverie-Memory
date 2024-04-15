bb98cba2f9defd5502b9c39c836c9a4672a96c3a
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-04-15 23:51:33 +1200

    WHY NO AUDIO WORK

--- scripts/past.gd
+++ scripts/past.gd
@@ -1,13 +1,5 @@
 extends Control
 
-@onready var homework_reminder = $HomeworkReminder
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	homework_reminder.play()
-
 
 func _on_homework_reminder_finished():
 	print("why not?")
-	homework_reminder.play()
