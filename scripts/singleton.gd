04e52510ce1942dfa2f55ba175dfe14c030ab511
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-04-29 22:00:54 +1200

    start overall winner label

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -4,3 +4,19 @@ class_name SingletonN
 var is_dragging := false
 var monitor_unlocked := false
 var homework_done := false
+
+var past_winning_label: Label
+var has_won_buttons := false
+var has_won_homework := false
+
+
+func win_homework() -> void:
+	if has_won_buttons:
+		past_winning_label.visible = true
+	has_won_homework = true
+
+
+func win_buttons() -> void:
+	if has_won_homework:
+		past_winning_label.visible = true
+	has_won_buttons = true
