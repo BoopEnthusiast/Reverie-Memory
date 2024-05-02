6523e8db1e6a292af2f677bcb765deece04bdd8a
ChaoticTiefling402 <Brookemd402@gmail.com>
2024-05-02 16:35:22 +1200

    fixed buttons

--- scripts/ColorButton.gd
+++ scripts/ColorButton.gd
@@ -9,7 +9,7 @@ var current_color = 1
 
 func _on_pressed():
 	if SingletonNode.has_won_homework:
-		color_changed.emit()
+	
 		
 		current_color += 1
 		for color in colors:
@@ -19,5 +19,5 @@ func _on_pressed():
 			current_color = 0
 		
 		colors[current_color].visible = true
-		
+		color_changed.emit()
 
