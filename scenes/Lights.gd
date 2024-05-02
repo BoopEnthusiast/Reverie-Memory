fd947226ee5c0eb22f573350a67cab401c5b498d
ChaoticTiefling402 <Brookemd402@gmail.com>
2024-05-02 16:11:35 +1200

    ColorRect and blank script for fairy lights

---
+++ scenes/Lights.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var color_rect = $ColorRect
+@onready var color_rect_2 = $ColorRect2
+@onready var color_rect_3 = $ColorRect3
+@onready var color_rect_4 = $ColorRect4
+@onready var color_rect_5 = $ColorRect5
+@onready var color_rect_6 = $ColorRect6
+
+#Check if coloured buttons are done, turn lights on (colorrect) 
+
