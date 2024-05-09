802a7ec054441234d2026c69c2e7bdad9a09666d
Boop <onpotis@gmail.com>
2024-05-09 15:34:21 +1200

    blacklight working!!

--- scripts/blacklight.gd
+++ scripts/blacklight.gd
@@ -1,6 +1,17 @@
 extends Sprite2D
 
 
+@onready var nums = $Nums
+
+var initial_child_global_position: Vector2
+
+
+func _ready():
+	initial_child_global_position = nums.global_position
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = get_global_mouse_position()
+	nums.global_position = initial_child_global_position
+	
--- scripts/cod line.gd
+++ scripts/cod line.gd
@@ -1,18 +1,18 @@
 extends Control
 
-@onready var odes: Array[ColorButton]  = [$Code, $Code2, $Code3, $Code4, $Code5, $Code6, $Code7, $Code8, $Code9, $Code10, $Code11, $Code12, $Code13, $Code14]
+@onready var nodes: Array[ColorButton]  = [$Code, $Code2, $Code3, $Code4, $Code5, $Code6, $Code7, $Code8, $Code9, $Code10, $Code11, $Code12, $Code13, $Code14]
 var what_colors_should_be: Array[int] = [1,1,2,3,2,1,2,3,3,2,1,3,2,1]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for button in odes:
+	for button in nodes:
 		button.color_changed.connect(color_changed)
 
 
 func color_changed():
 	var is_correct = true
 	var i = 0
-	for button in odes:
+	for button in nodes:
 		if not button.CurrentColour == what_colors_should_be[i]:
 			is_correct = false
 		i += 1
