698195e49e7d91b187621afac257975352566ab4
Boop <onpotis@gmail.com>
2024-05-21 16:39:59 +1200

    show future particle trails

--- scripts/monitor.gd
+++ scripts/monitor.gd
@@ -7,3 +7,11 @@ extends Button
 
 func _on_pressed():
 	monitor_screen.visible = true
+
+
+func _on_mouse_entered():
+	SingletonNode.cursor_trail_particles_node.enable()
+
+
+func _on_mouse_exited():
+	SingletonNode.cursor_trail_particles_node.disable()
