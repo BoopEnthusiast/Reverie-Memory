a4bfe87e4047a51370731e5efb17ed256b5ba250
ChaoticTiefling402 <Brookemd402@gmail.com>
2024-05-21 16:58:27 +1200

    lockbox 3

--- scripts/Box_folder.gd
+++ scripts/Box_folder.gd
@@ -13,18 +13,28 @@ func pressed_1():
 	if button_1.Current_Number == 4:
 		Button1 = true
 		print("4")
+	else: 
+		Button1 = false
+		
 
 func pressed_2():
 	if button_2.Current_Number == 2:
 		Button2 = true
 		print("2")
+	else: 
+		Button2 = false
+		
 
 func pressed_3():
 	if button_3.Current_Number == 9:
 		Button3 = true
 		print("9")
+	else: 
+		Button3 = false
+		
 
 func Check_If_True():
 	if Button1 and Button2 and Button3 == true:
 		inside_lock.visible = true
+
 
