ba6f4b71ebdf699de56be88311a830c024cd4e69
ChaoticTiefling402 <Brookemd402@gmail.com>
2024-05-30 16:05:58 +1200

    label

--- scripts/Box.gd
+++ scripts/Box.gd
@@ -1,10 +1,15 @@
 extends Button
 
 @onready var box = $"../Box_folder"
+@onready var homework_first = $"../Homework First"
+@onready var timer = $"../Book/Timer"
 
 func _on_pressed():
 	if SingletonNode.has_won_homework:
 		box.visible = true
+	else: 
+		homework_first.visible = true 
+		timer.start()
 
 
 func _on_mouse_entered():
@@ -13,3 +18,7 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	SingletonNode.cursor_trail_particles_node.disable()
+
+
+func _on_timer_timeout():
+	homework_first.visible = false
